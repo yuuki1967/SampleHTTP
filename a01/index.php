c226556bbf788b1bb45d03f5655a5249ec7b1568
yuuki1967 <yuki06.endo@gmail.com>
2021-12-22 09:10:52 +0900

    Comment

--- a01/index.php
+++ a01/index.php
@@ -5,7 +5,6 @@ $user = "suzuki";
 $keyword = "";
 if(isset($_GET['keyword'])){
 	$keyword = $_GET['keyword'];
-	//Comment.
 }
 $mysqli = new mysqli(DB_HOST.':'.DB_PORT, DB_USER, DB_PASS, DB_DATABASE);
 
